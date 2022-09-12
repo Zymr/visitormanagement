6b70940b70c1a47d610cca81e4097f442a7ae467
prerana dabhade <prerana.dabhade@zymr.com>
2022-09-12 16:55:13 +0530

    Upgrade config_env

--- src/main/java/com/zymr/zvisitor/resource/AppErrorController.java
+++ src/main/java/com/zymr/zvisitor/resource/AppErrorController.java
@@ -14,8 +14,8 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.boot.autoconfigure.web.ErrorAttributes;
-import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -23,10 +23,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class AppErrorController implements ErrorController{
+public class AppErrorController implements ErrorController {
 
     /**
      * Error Attributes in the Application
@@ -71,11 +72,9 @@ public class AppErrorController implements ErrorController{
      *
      * @return the error path
      */
-    @Override
     public String getErrorPath() {
         return ERROR_PATH;
     }
-
 
     private boolean getTraceParameter(HttpServletRequest request) {
         String parameter = request.getParameter("trace");
@@ -88,8 +87,8 @@ public class AppErrorController implements ErrorController{
     private Map<String, Object> getErrorAttributes(HttpServletRequest request,
                                                    boolean includeStackTrace) {
         RequestAttributes requestAttributes = new ServletRequestAttributes(request);
-        return this.errorAttributes.getErrorAttributes(requestAttributes,
-                includeStackTrace);
+        return this.errorAttributes.getErrorAttributes((WebRequest) request,
+                null);
     }
 
     private HttpStatus getStatus(HttpServletRequest request) {
