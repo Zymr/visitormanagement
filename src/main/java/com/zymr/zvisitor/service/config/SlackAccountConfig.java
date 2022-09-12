db45b1f82d9d838bf27ae97d372a594c111fffdc
prerana dabhade <prerana.dabhade@zymr.com>
2022-09-12 17:21:16 +0530

    Upgrade config_env

--- src/main/java/com/zymr/zvisitor/service/config/SlackAccountConfig.java
+++ src/main/java/com/zymr/zvisitor/service/config/SlackAccountConfig.java
@@ -10,7 +10,8 @@
 
 package com.zymr.zvisitor.service.config;
 
-import org.hibernate.validator.constraints.NotBlank;
+
+import javax.validation.constraints.NotBlank;
 
 public class SlackAccountConfig {
 	
