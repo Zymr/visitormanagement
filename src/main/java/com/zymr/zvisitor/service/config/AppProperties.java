980c78a7d0fe3432518f804f60b6ce98e74d68c2
prerana dabhade <prerana.dabhade@zymr.com>
2022-09-12 16:57:44 +0530

    Upgrade config_env

--- src/main/java/com/zymr/zvisitor/service/config/AppProperties.java
+++ src/main/java/com/zymr/zvisitor/service/config/AppProperties.java
@@ -16,11 +16,11 @@ import java.util.Map;
 import java.util.Objects;
 
 import javax.annotation.PostConstruct;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
-import org.hibernate.validator.constraints.NotBlank;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
